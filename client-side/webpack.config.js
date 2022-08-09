e3f832ced8deb15f87344e890df81fe608ce9f81
tomerp_pepperi <tomer.p@pepperi.com>
2022-08-09 14:05:29 +0300

    no message

--- client-side/webpack.config.js
+++ client-side/webpack.config.js
@@ -1,7 +1,6 @@
 const { shareAll, share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 // file_name should be lowercase and if it more then one word put '_' between them,
 const addonConfig = require('../addon.config.json');
-// const filename = `file_${addonConfig.AddonUUID.replace(/-/g, '_').toLowerCase()}`;
 const filename = `file_${addonConfig.AddonUUID}`;
 
 const webpackConfig = withModuleFederationPlugin({
